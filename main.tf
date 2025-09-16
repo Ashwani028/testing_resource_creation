0829b870df0d2f310a55b37e1c514d2f78a80487
Ashwani <ajaiswal028@gmail.com>
2025-09-16 17:26:49 +0530

    add new rg

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 resource "azurerm_resource_group" "RG" {
   name     = "zxc1"
   location = "Korea South"
+}
+
+resource "azurerm_resource_group" "RG1" {
+  name     = "zxc22"
+  location = "Korea South"
 
 }
+
